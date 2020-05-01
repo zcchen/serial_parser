cf747655d158ba9b1d869baee9a24c84efd5f937
zcchen <chen.bill.bill@gmail.com>
2020-05-01 12:28:01 +0800

    Added SerialParser.find() function

--- python/serial_parser.py
+++ python/serial_parser.py
@@ -11,7 +11,9 @@ class SerialParserErrorNotCompleted(SerialParserError): pass
 
 class SerialParser:
 
-    def __init__(self, header='S'):
+    def __init__(self, header=b'S'):
+        if type(header) is not bytes:
+            raise TypeError("header should be byte type, but not <{}>".format(type(header)))
         self.__header = header
         self.__crc_func = crcmod.predefined.mkPredefinedCrcFun("crc-16")
 
@@ -25,12 +27,26 @@ class SerialParser:
     def _bytes(self, the_str):
         if type(the_str) is bytes:
             return the_str
+        if type(the_str) is int:
+            the_str = chr(the_str)
         if sys.version_info.major < 3:
             return bytes(the_str)
         elif sys.version_info.major == 3:
-            return bytes(the_str, 'ascii')
+            return bytes(the_str, 'latin')
         else:
             raise Exception("Python Version Error")
+
+    def _byte2int(self, byte):
+        if sys.version_info.major < 3:
+            if type(byte) is not bytes:
+                raise TypeError("byte <{the_byte}> is expected as <bytes>, but not {t}.".format(\
+                                the_byte=byte, t=type(byte)))
+            return ord(byte)
+        elif sys.version_info.major == 3:
+            return byte
+        else:
+            raise Exception("Python Version Error")
+
 
     def encode(self, payload):
         msg = b""
@@ -41,24 +57,49 @@ class SerialParser:
         return msg
 
     def decode(self, msg, max_msglen=0):
+        # return payload
         ret = self.__msg_check(msg, max_msglen=max_msglen)
         if ret >= 0:
             return msg[2:-2]
 
     def find(self, raw, max_msglen=0):
-        # return payload
-        raise NotImplementedError
+        # return (msg_this, msg_rest)
+        if type(raw) is not bytes:
+            raise TypeError("msg <{msg}> should be <bytes>, but not <{t}>".format(
+                            msg=raw, t=type(msg)))
+        first_header_index = 0
+        found_msglen = 0
+        while first_header_index < len(raw):
+            try:
+                found_msglen = self.__msg_check(raw[first_header_index:], max_msglen=max_msglen)
+                if found_msglen:
+                    first_msg = raw[first_header_index: first_header_index + found_msglen]
+                else:
+                    first_msg = b""
+                second_msg = raw[first_header_index + found_msglen:]
+                if not second_msg:
+                    second_msg = b""
+                return (first_msg, second_msg)
+            except SerialParserErrorHeaderUnmatch: pass
+            except SerialParserErrorCRC: pass
+            except SerialParserErrorHeaderFaked: pass
+            except SerialParserErrorNotCompleted:
+                return (raw[first_header_index:], b"")
+            except Exception as e:
+                raise e
+            first_header_index += 1
+        return (b"", b"")
 
     def __msg_check(self, msg, max_msglen=0):
         # return the length of the correct msg from the first byte
         if type(msg) is not bytes:
             raise TypeError("msg <{msg}> should be <bytes>, but not <{t}>".format(
                             msg=msg, t=type(msg)))
-        if struct.pack("B", msg[0]) != self.HEADER:
+        if self._bytes(msg[0]) != self.HEADER:
             raise SerialParserErrorHeaderUnmatch(\
                     "msg[0] is <{header_msg}>, but not <{header_def}>".format(
-                        header_msg=struct.pack("B", msg[0]), header_def=self.HEADER))
-        msglen = int(msg[1])    # should be >= 2
+                        header_msg=self._bytes(msg[0]), header_def=self.HEADER))
+        msglen = self._byte2int(msg[1])    # should be >= 2
         if max_msglen:
             if msglen + 2 > max_msglen:
                 raise SerialParserErrorHeaderFaked
--- python/tests/test_serial_parser.py
+++ python/tests/test_serial_parser.py
@@ -4,7 +4,8 @@ from ..serial_parser import SerialParser
 
 @pytest.fixture(scope="module")
 def serial_parser():
-    return SerialParser("S")
+    return SerialParser(b"S")
+
 
 payload_msg_testdata = [ # payload, msg
         (b"123", b'S\x05123\x80\x7a')
@@ -17,4 +18,38 @@ def test_encoder(serial_parser, payload, msg):
 @pytest.mark.parametrize("payload, msg", payload_msg_testdata)
 def test_decoder(serial_parser, payload, msg):
     assert payload == serial_parser.decode(msg)
+
+
+found_msg_testdata = [ # insert_msg, msg, append_msg, max_msglen=0
+        (b"ab", b'S\x05123\x80\x7a', b"cd", 0),
+    ]
+
+found_msg_but_not_complete_testdata = [ # insert_msg, msg, append_msg, max_msglen=0
+        (b"ab", b'S\x08123\x80\x7a', b"cd", 0),
+    ]
+
+found_msg_but_nothing_testdata = [ # insert_msg, msg, append_msg, max_msglen=0
+        (b"ab", b'S\x03123\x80\x7a', b"cd", 0),
+    ]
+
+@pytest.mark.parametrize("insert_msg, msg, append_msg, max_msglen",
+                         found_msg_testdata)
+def test_find(serial_parser, insert_msg, msg, append_msg, max_msglen):
+    found = serial_parser.find(insert_msg + msg + append_msg, max_msglen=max_msglen)
+    assert msg == found[0]
+    assert append_msg == found[1]
+
+@pytest.mark.parametrize("insert_msg, msg, append_msg, max_msglen",
+                         found_msg_but_not_complete_testdata)
+def test_find_but_not_complete(serial_parser, insert_msg, msg, append_msg, max_msglen):
+    found = serial_parser.find(insert_msg + msg + append_msg, max_msglen=max_msglen)
+    assert msg + append_msg == found[0]
+    assert b"" == found[1]
+
+@pytest.mark.parametrize("insert_msg, msg, append_msg, max_msglen",
+                         found_msg_but_nothing_testdata)
+def test_find_but_nothing(serial_parser, insert_msg, msg, append_msg, max_msglen):
+    found = serial_parser.find(insert_msg + msg + append_msg, max_msglen=max_msglen)
+    assert b"" == found[0]
+    assert b"" == found[1]
 
